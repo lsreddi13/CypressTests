67dee7037354ea868e1ac713c837eedccb09398c
SasidharReddy <sasidharreddilingala@gmail.com>
2020-06-09 13:55:55 +0530

    day1 practise

---
+++ testme.js
@@ -0,0 +1,39 @@
+describe("Login functionality", function(){
+// it is a testcase
+// you can ghave multiple testcases by using multiple it blocks
+    it("Logon with valid creadential", function(){
+        //print statment
+        console.log("My First Log")
+    })
+
+    it("Logon with valid creadential", function(){
+        //print statment
+        console.log("My First Log")
+    })
+    describe("inside Login functionality", function(){
+        // it is a testcase
+        // you can ghave multiple testcases by using multiple it blocks
+            it("Logon with valid creadential", function(){
+                //print statment
+                console.log("My First Log")
+            })
+        
+            it("Logon with valid creadential", function(){
+                //print statment
+                console.log("My First Log")
+            })
+        })
+})
+describe("Login2 functionality", function(){
+    // it is a testcase
+    // you can ghave multiple testcases by using multiple it blocks
+        it("Logon with valid creadential", function(){
+            //print statment
+            console.log("My First Log")
+        })
+    
+        it("Logon with valid creadential", function(){
+            //print statment
+            console.log("My First Log")
+        })
+    })
---
+++ testme2.js
@@ -0,0 +1,27 @@
+describe("Login functionality", function(){
+
+    before(function(){
+        console.log("This is before -----")
+    });
+    after(function() {
+        console.log("this is after -----")
+    });
+    afterEach(function(){
+        console.log("after each +++++++++++++++")
+    });
+    beforeEach(function(){
+        console.log("before each ++++++++++++++")
+    })
+// it is a testcase
+// you can ghave multiple testcases by using multiple it blocks
+    it("Logon with valid creadential", function(){
+        //print statment
+        console.log("My First Log")
+    })
+
+    it("Logon with valid creadential", function(){
+        //print statment
+        console.log("My First Log")
+    })
+    
+})
